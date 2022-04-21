efb1b9d77e9d92480837b9d27ea317b6c07ce2ea
Danil <danil.cher57@gmail.com>
2022-04-21 23:46:25 +0300

    Model MeetUp

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace CRUD_WEB_API.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<MeetUp> MeetUps { get; set; }
+        public ApplicationDbContext(DbContextOptions options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MeetUp>(entity =>
+            {
+                entity.ToTable("MeetUp");
+
+                entity.Property(e => e.Id).ValueGeneratedNever();
+
+                entity.Property(e => e.Date).HasColumnType("timestamp without time zone");
+            });
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace CRUD_WEB_API.Models
+{
+    public class User : IdentityUser
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+    }
+}
