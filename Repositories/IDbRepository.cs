57448d96d7472e6e4de134e9b8842003e44c6521
Danil <danil.cher57@gmail.com>
2022-04-21 23:50:25 +0300

    Repository

---
+++ Repositories/IDbRepository.cs
@@ -0,0 +1,16 @@
+﻿using CRUD_WEB_API.Models;
+using CRUD_WEB_API.Paginations;
+using CRUD_WEB_API.SortingModels;
+
+namespace CRUD_WEB_API.Repositories
+{
+    public interface IDbRepository<T> where T : class
+    {
+        IEnumerable<T> GetList();
+        T Get(int id);
+        void Create(T item);
+        void Update(T item);
+        T Delete(int id);
+        IEnumerable<MeetUp> GetWithOptions(Pagination pagination, SortingOptions sortingOptions, string subString);
+    }
+}
