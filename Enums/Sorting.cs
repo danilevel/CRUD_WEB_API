f78664fa1accfac26cc647b27afdb2a7d0c2cc93
Danil <danil.cher57@gmail.com>
2022-04-21 23:55:42 +0300

    Enum

---
+++ Enums/Sorting.cs
@@ -0,0 +1,12 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CRUD_WEB_API.SortingModels
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum Sorting
+    {
+        Nothing,
+        Asc,
+        Desc
+    }
+}
