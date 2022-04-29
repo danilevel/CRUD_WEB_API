ff6450d3ca4a9069aba5ea21108e5c4d4de393c0
Danil <danil.cher57@gmail.com>
2022-04-29 17:17:31 +0300

    Add project to GIT

---
+++ ApplicationDbContext.cs
@@ -0,0 +1,51 @@
+﻿using CRUD_WEB_API.DTO;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_WEB_API
+{
+    public partial class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<MeetUp> MeetUps { get; set; } = null!;
+        public virtual DbSet<User> Users { get; set; } = null!;
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<MeetUp>(entity =>
+            {
+                entity.ToTable("MeetUp");
+
+                entity.Property(e => e.Date).HasColumnType("timestamp without time zone");
+            });
+
+            modelBuilder.Entity<User>(entity =>
+            {
+                entity.ToTable("User");
+
+                entity.HasMany(d => d.Meetups)
+                    .WithMany(p => p.Users)
+                    .UsingEntity<Dictionary<string, object>>(
+                        "Usersmeetup",
+                        l => l.HasOne<MeetUp>().WithMany().HasForeignKey("Meetupid").OnDelete(DeleteBehavior.ClientSetNull).HasConstraintName("usersmeetups_meetupid_fkey"),
+                        r => r.HasOne<User>().WithMany().HasForeignKey("Userid").OnDelete(DeleteBehavior.ClientSetNull).HasConstraintName("usersmeetups_userid_fkey"),
+                        j =>
+                        {
+                            j.HasKey("Userid", "Meetupid").HasName("users_meetups_pkey");
+
+                            j.ToTable("usersmeetups");
+
+                            j.IndexerProperty<int>("Userid").HasColumnName("userid");
+
+                            j.IndexerProperty<int>("Meetupid").HasColumnName("meetupid");
+                        });
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
